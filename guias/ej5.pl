a71eb1c08f20c331dd098b93bfbd5b040d7c984d
Federico Collasius <fede.collasius@gmail.com>
2025-11-15 20:12:48 -0300

    feat: ej5

--- guias/ej5.pl
+++ guias/ej5.pl
@@ -10,4 +10,18 @@ reverse_([L|LS], R) :-
 % reverso([3, 4, 5])
 % reverso([1, 2], [2, 1])
 
+% prefijo(?P, +L)
+prefijo(P, L) :- append(P, _, L).
 
+% sufijo(?S, +L)
+sufijo(S, L) :- append(_, S, L).
+
+% L = [1,2,3,4,5]
+% L = [1,2] ++ [3,4,5]
+% S = [3,4] ++ [5]
+% sublista(?S, +L)
+sublista([], _).
+sublista(S, L) :- append(P, _, L), append(_, S, P).
+
+% pertenece(?X, +L)
+pertenece(X, L) :- sublista([X], L).
