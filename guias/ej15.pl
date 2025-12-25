30ce0083795dfa55673d6bcc8604ae062252b331
Federico Collasius <fede.collasius@gmail.com>
2025-12-25 01:25:27 -0300

    feat: ej15

--- guias/ej15.pl
+++ guias/ej15.pl
@@ -3,6 +3,9 @@
 % Triangulo valido <-> 
 %	- cada lado menor que suma de los otros dos y mayor que su diferencia
 
+desde(X, X).
+desde(X, Y) :- N is X+1, desde(N, Y).
+
 esTriangulo(tri(A,B,C)) :-
   A > 0,
   B > 0,
@@ -31,6 +34,10 @@ armarTriplas(P,A,B,C) :-
 	between(1,P,A),
 	LB is P - A, between(1,LB,B),
 	C is P -A -B.
+
+generarTriplas(A, B, C) :-
+  desdeRev(1, N), between(1, N, A), LB is N - A, between(1, LB, B), C is N - A - B.
+
 
 % triangulo(-T): genera triangulos validos sin repetir resultados
 
